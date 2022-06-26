b5884e8f18fde070c41d5348d8a81da70de4558f
nasrah01 <nasrah1@hotmail.co.uk>
2022-06-26 16:51:34 +0100

    config

--- index.js
+++ index.js
@@ -9,19 +9,9 @@ import authRoute from './routes/authRoutes.js'
 const app = express()
 const PORT = process.env.PORT || 5000;
 
-const whitelist = [
-  "http://localhost:3000",
-];
-const corsOptions = {
-  credentials: true,
-  origin: (origin, callback) => {
-    if (whitelist.includes(origin)) return callback(null, true);
 
-    callback(new Error("Not allowed by CORS"));
-  },
-};
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
