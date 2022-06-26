1f86c59e2aa0a06a78e24b423b331601a409bc38
nasrah01 <nasrah1@hotmail.co.uk>
2022-06-26 17:24:13 +0100

    config

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 dotenv.config()
 import cors from "cors";
+import helmet from "helmet";
 import cookieParser from 'cookie-parser';
 import connectDB from './db/connect.js'
 import authRoute from './routes/authRoutes.js'
@@ -9,9 +10,20 @@ import authRoute from './routes/authRoutes.js'
 const app = express()
 const PORT = process.env.PORT || 5000;
 
+const whitelist = [
+  "http://localhost:3000",
+  "https://grand-lokum-45486f.netlify.app/",
+];
+const corsOptions = {
+  credentials: true,
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1) return callback(null, true);
 
-
-app.use(cors());
+    callback(new Error("Not allowed by CORS"));
+  },
+};
+app.use(helmet());
+app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
