b73d1646134798ea902049d593530be8d8e486e0
nasrah01 <nasrah1@hotmail.co.uk>
2022-06-26 17:41:18 +0100

    config

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 const whitelist = [
   "http://localhost:3000",
+  "http://localhost:5000",
   "https://grand-lokum-45486f.netlify.app/",
 ];
 const corsOptions = {
